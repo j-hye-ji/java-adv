4d6422151c471408ec0da328acc01c3d3c914452
Junghyeji <arang030@naver.com>
2025-05-25 16:06:21 +0900

    스레드 시작 1

---
+++ src/thread/start/HelloThreadMain.java
@@ -0,0 +1,14 @@
+package thread.start;
+
+public class HelloThreadMain {
+    public static void main(String[] args) {
+        System.out.println(Thread.currentThread().getName() + ": main() start");
+
+        HelloThread helloThread = new HelloThread();
+        System.out.println(Thread.currentThread().getName() + ": start() 호출 전");
+        helloThread.start(); /* run() 메서드 실행 x 반드시 start() 메서드 실행 */
+        System.out.println(Thread.currentThread().getName() + ": start() 호출 후");
+
+        System.out.println(Thread.currentThread().getName() + ": main() end");
+    }
+}
